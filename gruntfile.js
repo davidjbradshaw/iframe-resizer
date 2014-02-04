e3068965e4064a8be4d571f40e15b0ec3b65065a
David J. Bradshaw <dave@bradshaw.net>
2014-02-04 23:00:47 +0000

    Release v1.3.4

--- gruntfile.js
+++ gruntfile.js
@@ -108,7 +108,7 @@ module.exports = function(grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo: 'orgin',
+        pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
       }
     }
