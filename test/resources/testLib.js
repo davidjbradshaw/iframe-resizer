830b4f3dc2f4ae16395f1a9687e0a61e93ef29ec
David J. Bradshaw <dave@bradshaw.net>
2015-09-02 12:40:27 +0100

    Disable log msgs

--- test/resources/testLib.js
+++ test/resources/testLib.js
@@ -4,6 +4,6 @@ function sendMessage(msg){
 
 	//document.getElementsByTagName('iframe')[0].contentWindow.postMessage( msgId + msg, '*' );
 
-	console.log('Sending '+msg);
+	//console.log('Sending '+msg);
 	document.getElementsByTagName('iframe')[0].iFrameResizer.sendMessage(msg);
 }
