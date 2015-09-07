86b034b6aad4e3d6bbfe55187fb964c491484b04
David J. Bradshaw <dave@bradshaw.net>
2015-09-07 17:12:52 +0100

    Add progess reporter

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['logcapture', 'verbose-summary', 'coverage'],
+    reporters: ['logcapture', 'progress', 'verbose-summary', 'coverage'],
 
 
     // web server port
