18c912b7fdeff2944b4d107f66dea8d37a6f2d59
David J. Bradshaw <dave@bradshaw.net>
2015-10-01 11:52:39 +0100

    Add morked parent test for getPageInfo #262

---
+++ spec/getPageInfoSpec.js
@@ -0,0 +1,29 @@
+define(['iframeResizer'], function(iFrameResize) {
+
+	describe('Get Page info', function() {
+		var log=LOG;
+		var testId = 'anchor';
+
+		beforeEach(function(){
+			loadIFrame('iframe600.html');
+		});
+
+		it('requested from iFrame', function(done) {
+			var iframe1 = iFrameResize({
+				log:log,
+				id:'getPageInfo',
+			})[0];
+
+			spyOn(iframe1.contentWindow,'postMessage').and.callFake(function(msg) {
+				if(0!==msg.indexOf('pageInfo')){
+					expect(msg.indexOf('"offsetTop":0,"offsetLeft":0,"scrollTop":0,"scrollLeft":0')).not.toEqual(0);
+					tearDown(iframe1);
+					done();
+				}
+			});
+
+			mockMsgFromIFrame(iframe1,'pageInfo');
+		});
+
+	});
+});
