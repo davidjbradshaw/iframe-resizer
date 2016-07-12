5d6a22d880d889ee464437ed015f46f76eafa616
Jeff <jhicken@gmail.com>
2016-07-12 11:25:44 -0600

    Fixing disconnect when iframe is missing

--- js/iframeResizer.js
+++ js/iframeResizer.js
@@ -271,8 +271,8 @@
 			function debouncedTrigger(){
 				trigger(
 					'Send Page Info',
-					'pageInfo:' + getPageInfo(), 
-					iframe, 
+					'pageInfo:' + getPageInfo(),
+					iframe,
 					iframeId
 				);
 			}
@@ -304,7 +304,7 @@
 			function start(){
 				setListener('Add ', addEventListener);
 			}
-			
+
 			var id = iframeId; //Create locally scoped copy of iFrame ID
 
 			start();
@@ -628,9 +628,6 @@
 
 		function iFrameNotFound(){
 			info(id,'[' + calleeMsg + '] IFrame('+id+') not found');
-			if(settings[id]) {
-				delete settings[id];
-			}
 		}
 
 		function chkAndSend(){
