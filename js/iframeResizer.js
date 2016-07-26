61acf0cfbaad498336e44bc184274a9f0c7ae7ca
Jeff <jhicken@gmail.com>
2016-07-26 11:32:55 -0600

    fix the close function when the iframe is not in the DOM

--- js/iframeResizer.js
+++ js/iframeResizer.js
@@ -533,7 +533,7 @@
 		var iframeId = iframe.id;
 
 		log(iframeId,'Removing iFrame: '+iframeId);
-		iframe.parentNode.removeChild(iframe);
+		if (iframe.parentNode) { iframe.parentNode.removeChild(iframe); }
 		chkCallback(iframeId,'closedCallback',iframeId);
 		log(iframeId,'--');
 		delete settings[iframeId];
