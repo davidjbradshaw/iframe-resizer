7421022b2534d29c39f9eb56e1ab5f0d2f74fb35
David J. Bradshaw <dave@bradshaw.net>
2018-01-21 15:52:15 +0000

    Tabs -> 2 Spaces

--- src/ie8.polyfils.js
+++ src/ie8.polyfils.js
@@ -5,20 +5,20 @@
  * https://developer.mozilla.org/
  */
 
- if (!Array.prototype.forEach){
-	Array.prototype.forEach = function(fun /*, thisArg */){
-		"use strict";
-		if (this === void 0 || this === null || typeof fun !== "function") throw new TypeError();
+if (!Array.prototype.forEach){
+  Array.prototype.forEach = function(fun /*, thisArg */){
+    "use strict";
+    if (this === void 0 || this === null || typeof fun !== "function") throw new TypeError();
 
-		var
-			t = Object(this),
-			len = t.length >>> 0,
-			thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    var
+      t = Object(this),
+      len = t.length >>> 0,
+      thisArg = arguments.length >= 2 ? arguments[1] : void 0;
 
-		for (var i = 0; i < len; i++)
-			if (i in t)
-				fun.call(thisArg, t[i], i, t);
-	};
+    for (var i = 0; i < len; i++)
+      if (i in t)
+        fun.call(thisArg, t[i], i, t);
+  };
 }
 
 
