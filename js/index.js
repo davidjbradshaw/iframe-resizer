e2259be0bd9ced36ae58132c830c4191c576fd54
David J. Bradshaw <dave@bradshaw.net>
2019-09-16 12:01:31 +0100

    Fix export

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
-exports.iframeResizer = require('./iframeResizer')
+const iframeResize = require('./iframeResizer')
+
+exports.iframeResize = iframeResize
+exports.iframeResizer = iframeResize // Backwards compatability
 exports.iframeResizerContentWindow = require('./iframeResizer.contentWindow')
