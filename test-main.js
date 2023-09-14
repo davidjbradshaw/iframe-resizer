f60afeaf0ef8062b8ac1a66e979436d2c3ffe7f7
David J. Bradshaw <dave@bradshaw.net>
2023-09-14 18:05:33 +0100

    Update build deps

--- test-main.js
+++ test-main.js
@@ -9,7 +9,7 @@ Object.keys(window.__karma__.files).forEach(function (file) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
+    var normalizedTestModule = file.replaceAll(/^\/base\/|\.js$/g, '')
     allTestFiles.push(normalizedTestModule)
   }
 })
