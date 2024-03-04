e397c90c5cca01169fbf5a1a688a6c1d87b9e93d
David J. Bradshaw <dave@bradshaw.net>
2024-03-04 22:34:26 +0000

    tidy

--- build/pkg.js
+++ build/pkg.js
@@ -1,24 +1,36 @@
 const main = (file) =>
-  file === 'parent' ? {
-    main: "iframe-resizer.parent.cjs",
-    module: "iframe-resizer.parent.mjs",
-    browser: "iframe-resizer.parent.js",
-  } : {
-    main: `iframer-resizer.${file}.js`,
-  }
+  file === 'parent'
+    ? {
+        main: 'iframe-resizer.parent.cjs',
+        module: 'iframe-resizer.parent.mjs',
+        browser: 'iframe-resizer.parent.js',
+      }
+    : {
+        main: `iframe-resizer.${file}.js`,
+      }
 
-export default (file) => (pkg) => ({
-name: "@iframe-resizer/" + file,
-version: pkg.version,
-license: pkg.license,
-private: false,
-homepage: pkg.homepage,
-author: pkg.author,
-description: pkg.description,
-github: pkg.github,
-repository: pkg.repository,
-funding: pkg.funding,
-keywords: pkg.keywords,
-...main(file),
-// "files": ["."]
-})
+export default (file) =>
+  ({
+    version,
+    license,
+    homepage,
+    author,
+    description,
+    github,
+    repository,
+    funding,
+    keywords,
+  }) => ({
+    name: `@iframe-resizer/${file}`,
+    version,
+    license,
+    private: false,
+    homepage,
+    author,
+    description,
+    github,
+    repository,
+    funding,
+    keywords: [...keywords, file],
+    ...main(file),
+  })
