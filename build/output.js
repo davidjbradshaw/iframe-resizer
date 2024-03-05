bc2ab40cc04737e32d51b4bf97aee2ae5be5cb51
David J. Bradshaw <dave@bradshaw.net>
2024-03-05 09:10:08 +0000

    Refactor build script

---
+++ build/output.js
@@ -0,0 +1,25 @@
+// import BANNER from './banner.js'
+
+const paths = {
+    parent: 'dist/parent/',
+    child: 'dist/child/',
+    jquery: 'dist/jquery/',
+  }
+
+const ext = format => format === 'es' ? 'mjs' : format === 'umd' ? 'js' : format
+
+export const output = (file) => (format) => ({
+  // banner: BANNER[file],
+  file: paths[file] + `iframe-resizer.${file}.${ext(format)}`,
+  format,
+  sourcemap: false,
+})
+
+export const outputs = (file) => {
+  const out = output(file)
+  return [
+    out('es'),
+    out('cjs'),
+    out('umd'),
+  ]
+}
