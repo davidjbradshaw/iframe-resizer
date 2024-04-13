d95449e4896e7b2fdd6b53607a421980915a446b
David J. Bradshaw <dave@bradshaw.net>
2024-04-13 09:27:58 +0100

    Add pubSub code

---
+++ packages/common/pubSub.js
@@ -0,0 +1,31 @@
+export default {
+  events: {},
+
+  has(event) {
+    return typeof this.events[event] === 'object'
+  },
+
+  addListener(event, listener) {
+    if (!this.has(event)) this.events[event] = []
+    this.events[event].push(listener)
+    return () => this.removeListener(event, listener)
+  },
+
+  removeListener(event, listener) {
+    if (!this.has(event)) return
+    const idx = this.events[event].indexOf(listener)
+    if (idx === -1) return
+    this.events[event].splice(idx, 1)
+  },
+
+  emit(event, ...args) {
+    this.events[event]?.forEach((listener) => listener(...args))
+  },
+
+  once(event, listener) {
+    const remove = this.on(event, (...args) => {
+      remove()
+      listener(...args)
+    })
+  },
+}
