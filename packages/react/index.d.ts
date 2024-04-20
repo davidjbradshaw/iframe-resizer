51abc72f35866dd4a9fc15eebe156ad0778cb880
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2024-04-20 17:52:36 -0600

    fix: react types

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -1,57 +1,52 @@
-declare module "iframe-resizer-react" {
-  import * as React from "react";
+import * as React from 'react'
 
-  namespace IframeResizer {
-    type IFrameObject = {
-      close: () => void;
-      moveToAnchor: (anchor: string) => void;
-      resize: () => void;
-      sendMessage: (message: any, targetOrigin?: string) => void;
-      removeListeners: () => void;
-    };
-    interface IFrameComponent extends HTMLIFrameElement {
-      iFrameResizer: IFrameObject;
-    }
-
-    type IframeProps = React.DetailedHTMLProps<
-      React.IframeHTMLAttributes<HTMLIFrameElement>,
-      HTMLIFrameElement
-    >;
-
-    type ResizerOptions = {
-      bodyBackground?: string | null;
-      bodyMargin?: string | number | null;
-      bodyPadding?: string | number | null;
-      checkOrigin?: boolean | string[];
-      direction?: "vertical" | "horizontal" | "none";
-      forwardRef?: any;
-      inPageLinks?: boolean;
-      enablePublicMethods?: boolean;
-      offsetHeight?: number;
-      offsetWidth?: number;
-      scrolling?: boolean | "omit";
-      tolerance?: number;
-    };
-
-    type ResizerEvents = {
-      onInit?: (iframe: IFrameComponent) => void;
-      onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void;
-      onResized?: (ev: {
-        iframe: IFrameComponent;
-        height: number;
-        width: number;
-        type: string;
-      }) => void;
-      onScroll?: (ev: { x: number; y: number }) => boolean;
-    };
-
-    type IframeResizerProps = Omit<IframeProps, "scrolling"> &
-      ResizerOptions &
-      ResizerEvents;
-  }
-
-  function IframeResizer(
-    props: IframeResizer.IframeResizerProps
-  ): React.ReactElement;
-  export = IframeResizer;
+export type IFrameObject = {
+  close: () => void
+  moveToAnchor: (anchor: string) => void
+  resize: () => void
+  sendMessage: (message: any, targetOrigin?: string) => void
+  removeListeners: () => void
 }
+export interface IFrameComponent extends HTMLIFrameElement {
+  iFrameResizer: IFrameObject
+}
+
+export type IframeProps = React.DetailedHTMLProps<
+  React.IframeHTMLAttributes<HTMLIFrameElement>,
+  HTMLIFrameElement
+>
+
+export type ResizerOptions = {
+  bodyBackground?: string | null
+  bodyMargin?: string | number | null
+  bodyPadding?: string | number | null
+  checkOrigin?: boolean | string[]
+  direction?: 'vertical' | 'horizontal' | 'none'
+  forwardRef?: any
+  inPageLinks?: boolean
+  enablePublicMethods?: boolean
+  offsetHeight?: number
+  offsetWidth?: number
+  scrolling?: boolean | 'omit'
+  tolerance?: number
+}
+
+export type ResizerEvents = {
+  onInit?: (iframe: IFrameComponent) => void
+  onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void
+  onResized?: (ev: {
+    iframe: IFrameComponent
+    height: number
+    width: number
+    type: string
+  }) => void
+  onScroll?: (ev: { x: number; y: number }) => boolean
+}
+
+export type IframeResizerProps = Omit<IframeProps, 'scrolling'> &
+  ResizerOptions &
+  ResizerEvents
+
+export default function IframeResizer(
+  props: IframeResizerProps,
+): React.ReactElement
