59a8a1c7199425f422f3ecb9f353050b19ac0bee
David J. Bradshaw <dave@bradshaw.net>
2024-04-22 10:29:03 +0100

    #1228 Fix types - Alpha.8

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -1,57 +1,57 @@
-declare module "iframe-resizer-react" {
-  import * as React from "react";
+declare module '@iframe-resizer/react' {
+  import * as React from 'react'
 
   namespace IframeResizer {
     type IFrameObject = {
-      close: () => void;
-      moveToAnchor: (anchor: string) => void;
-      resize: () => void;
-      sendMessage: (message: any, targetOrigin?: string) => void;
-      removeListeners: () => void;
-    };
+      close: () => void
+      moveToAnchor: (anchor: string) => void
+      resize: () => void
+      sendMessage: (message: any, targetOrigin?: string) => void
+      removeListeners: () => void
+    }
     interface IFrameComponent extends HTMLIFrameElement {
-      iFrameResizer: IFrameObject;
+      iFrameResizer: IFrameObject
     }
 
     type IframeProps = React.DetailedHTMLProps<
       React.IframeHTMLAttributes<HTMLIFrameElement>,
       HTMLIFrameElement
-    >;
+    >
 
     type ResizerOptions = {
-      bodyBackground?: string | null;
-      bodyMargin?: string | number | null;
-      bodyPadding?: string | number | null;
-      checkOrigin?: boolean | string[];
-      direction?: "vertical" | "horizontal" | "none";
-      forwardRef?: any;
-      inPageLinks?: boolean;
-      enablePublicMethods?: boolean;
-      offsetHeight?: number;
-      offsetWidth?: number;
-      scrolling?: boolean | "omit";
-      tolerance?: number;
-    };
+      bodyBackground?: string | null
+      bodyMargin?: string | number | null
+      bodyPadding?: string | number | null
+      checkOrigin?: boolean | string[]
+      direction?: 'vertical' | 'horizontal' | 'none'
+      forwardRef?: any
+      inPageLinks?: boolean
+      enablePublicMethods?: boolean
+      offsetHeight?: number
+      offsetWidth?: number
+      scrolling?: boolean | 'omit'
+      tolerance?: number
+    }
 
     type ResizerEvents = {
-      onInit?: (iframe: IFrameComponent) => void;
-      onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void;
+      onInit?: (iframe: IFrameComponent) => void
+      onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void
       onResized?: (ev: {
-        iframe: IFrameComponent;
-        height: number;
-        width: number;
-        type: string;
-      }) => void;
-      onScroll?: (ev: { x: number; y: number }) => boolean;
-    };
+        iframe: IFrameComponent
+        height: number
+        width: number
+        type: string
+      }) => void
+      onScroll?: (ev: { x: number; y: number }) => boolean
+    }
 
-    type IframeResizerProps = Omit<IframeProps, "scrolling"> &
+    type IframeResizerProps = Omit<IframeProps, 'scrolling'> &
       ResizerOptions &
-      ResizerEvents;
+      ResizerEvents
   }
 
   function IframeResizer(
-    props: IframeResizer.IframeResizerProps
-  ): React.ReactElement;
-  export = IframeResizer;
+    props: IframeResizer.IframeResizerProps,
+  ): React.ReactElement
+  export = IframeResizer
 }
