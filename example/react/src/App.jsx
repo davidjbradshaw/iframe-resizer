3a996807f474e50f9ee977c0eb4a206c28280b3b
David J. Bradshaw <dave@bradshaw.net>
2024-05-13 12:56:26 +0100

    Update react example

--- example/react/src/App.jsx
+++ example/react/src/App.jsx
@@ -1,16 +1,34 @@
+import { useRef, useState } from 'react'
 import IframeResizer from '@iframe-resizer/react'
+
+import MessageData from './message-data.jsx'
 
 import './App.css'
 
 function App() {
+  const iframeRef = useRef(null)
+  const [messageData, setMessageData] = useState()
+
+  const onResized = (data) => setMessageData(data)
+
+  const onMessage = (data) => {
+    setMessageData(data)
+    alert(`Message from frame ${data.iframe.id}: ${data.message}`)
+    iframeRef.current.sendMessage('Hello back from the parent page')
+  }
+
   return (
     <>
-      <h2>@iframe-resizer/react test page</h2>
       <IframeResizer
         license="GPL-V3"
-        src="/child/frame.content.html"
-        style={{ width: '100%' }}  
+        forwardRef={iframeRef}
+        inPageLinks
+        onMessage={onMessage}
+        onResized={onResized}
+        src="child/frame.content.html"
+        style={{ width: '100%', height: '100vh'}}
       />
+      <MessageData data={messageData} />
     </>
   )
 }
