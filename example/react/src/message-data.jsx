dab74edd99a776d2dc6077a758f11430c3ec2bb9
David J. Bradshaw <dave@bradshaw.net>
2024-05-13 12:56:26 +0100

    Update react example

---
+++ example/react/src/message-data.jsx
@@ -0,0 +1,19 @@
+const MessageData = ({ data }) => {
+  return data ? (
+    data.message ? (
+      <span>
+        <b>Frame ID:</b> {data.iframe.id} <br/>
+        <b>Message:</b> {data.message}
+      </span>
+    ) : (
+      <span>
+        <b>Frame ID:</b> {data.iframe.id} <br/>
+        <b>Height:</b> {data.height} <br/>
+        <b>Width:</b> {data.width} <br/>
+        <b>Event type:</b> {data.type}
+      </span>
+    )
+  ) : null
+}
+
+export default MessageData
