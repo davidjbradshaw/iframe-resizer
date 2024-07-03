f48823b18a5772400482d543010fdc43d9937d92
André Abt <info@andre-abt.com>
2024-07-03 10:37:33 +0200

    Remove unnecessary module declarations

--- packages/child/index.d.ts
+++ packages/child/index.d.ts
@@ -8,127 +8,125 @@
  * I'm not a TypeScript dev, so please feel free to submit PRs to improve this file.
  */
 
-declare module '@iframe-resizer/child' {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  export interface IFramePageOptions {
-    /**
-     * This option allows you to restrict the domain of the parent page,
-     * to prevent other sites mimicking your parent page.
-     */
-    targetOrigin?: string | undefined
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface IFramePageOptions {
+  /**
+   * This option allows you to restrict the domain of the parent page,
+   * to prevent other sites mimicking your parent page.
+   */
+  targetOrigin?: string | undefined
 
-    /**
-     * Receive message posted from the parent page with the iframe.iFrameResizer.sendMessage() method.
-     */
-    onMessage?(message: any): void
+  /**
+   * Receive message posted from the parent page with the iframe.iFrameResizer.sendMessage() method.
+   */
+  onMessage?(message: any): void
 
-    /**
-     * This function is called once iFrame-Resizer has been initialized after receiving a call from the parent page.
-     */
-    onReady?(): void
+  /**
+   * This function is called once iFrame-Resizer has been initialized after receiving a call from the parent page.
+   */
+  onReady?(): void
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface IFramePage {
+  /**
+   * Turn autoResizing of the iFrame on and off. Returns bool of current state.
+   */
+  autoResize(resize?: boolean): boolean
+
+  /**
+   * Remove the iFrame from the parent page.
+   */
+  close(): void
+
+  /**
+   * Returns the ID of the iFrame that the page is contained in.
+   */
+  getId(): string
+
+  /**
+   * Ask the containing page for its positioning coordinates.
+   *
+   * Your callback function will be recalled when the parent page is scrolled or resized.
+   *
+   * Pass false to disable the callback.
+   */
+  getParentProps(callback: (data: ParentProps) => void): void
+
+  /**
+   * Scroll the parent page by x and y
+   */
+  scrollBy(x: number, y: number): void
+
+  /**
+   * Scroll the parent page to the coordinates x and y
+   */
+  scrollTo(x: number, y: number): void
+
+  /**
+   * Scroll the parent page to the coordinates x and y relative to the position of the iFrame.
+   */
+  scrollToOffset(x: number, y: number): void
+
+  /**
+   * Send data to the containing page, message can be any data type that can be serialized into JSON. The `targetOrigin`
+   * option is used to restrict where the message is sent to; to stop an attacker mimicking your parent page.
+   * See the MDN documentation on postMessage for more details.
+   */
+  sendMessage(message: any, targetOrigin?: string): void
+
+  /**
+   * Set default target origin.
+   */
+  setTargetOrigin(targetOrigin: string): void
+
+  /**
+   * Manually force iFrame to resize. To use passed arguments you need first to disable the `autoResize` option to
+   * prevent auto resizing and enable the `sizeWidth` option if you wish to set the width.
+   */
+  size(customHeight?: string, customWidth?: string): void
+}
+
+export interface ParentProps {
+  /**
+   * The values returned by iframe.getBoundingClientRect()
+   */
+  iframe: {
+    x: number
+    y: number
+    width: number
+    height: number
+    top: number
+    right: number
+    bottom: number
+    left: number
   }
 
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  export interface IFramePage {
-    /**
-     * Turn autoResizing of the iFrame on and off. Returns bool of current state.
-     */
-    autoResize(resize?: boolean): boolean
-
-    /**
-     * Remove the iFrame from the parent page.
-     */
-    close(): void
-
-    /**
-     * Returns the ID of the iFrame that the page is contained in.
-     */
-    getId(): string
-
-    /**
-     * Ask the containing page for its positioning coordinates.
-     *
-     * Your callback function will be recalled when the parent page is scrolled or resized.
-     *
-     * Pass false to disable the callback.
-     */
-    getParentProps(callback: (data: ParentProps) => void): void
-
-    /**
-     * Scroll the parent page by x and y
-     */
-    scrollBy(x: number, y: number): void
-
-    /**
-     * Scroll the parent page to the coordinates x and y
-     */
-    scrollTo(x: number, y: number): void
-
-    /**
-     * Scroll the parent page to the coordinates x and y relative to the position of the iFrame.
-     */
-    scrollToOffset(x: number, y: number): void
-
-    /**
-     * Send data to the containing page, message can be any data type that can be serialized into JSON. The `targetOrigin`
-     * option is used to restrict where the message is sent to; to stop an attacker mimicking your parent page.
-     * See the MDN documentation on postMessage for more details.
-     */
-    sendMessage(message: any, targetOrigin?: string): void
-
-    /**
-     * Set default target origin.
-     */
-    setTargetOrigin(targetOrigin: string): void
-
-    /**
-     * Manually force iFrame to resize. To use passed arguments you need first to disable the `autoResize` option to
-     * prevent auto resizing and enable the `sizeWidth` option if you wish to set the width.
-     */
-    size(customHeight?: string, customWidth?: string): void
+  /**
+   * The values returned by document.documentElement.scrollWidth and document.documentElement.scrollHeight
+   */
+  document: {
+    scrollWidth: number
+    scrollHeight: number
   }
 
-  export interface ParentProps {
-    /**
-     * The values returned by iframe.getBoundingClientRect()
-     */
-    iframe: {
-      x: number
-      y: number
-      width: number
-      height: number
-      top: number
-      right: number
-      bottom: number
-      left: number
-    }
-
-    /**
-     * The values returned by document.documentElement.scrollWidth and document.documentElement.scrollHeight
-     */
-    document: {
-      scrollWidth: number
-      scrollHeight: number
-    }
-
-    /**
-     * The values returned by window.visualViewport
-     */
-    viewport: {
-      width: number
-      height: number
-      offsetLeft: number
-      offsetTop: number
-      pageLeft: number
-      pageTop: number
-      scale: number
-    }
+  /**
+   * The values returned by window.visualViewport
+   */
+  viewport: {
+    width: number
+    height: number
+    offsetLeft: number
+    offsetTop: number
+    pageLeft: number
+    pageTop: number
+    scale: number
   }
+}
 
-  global {
-    interface Window {
-      iFrameResizer: IFramePageOptions
-      parentIFrame: IFramePage
-    }
+declare global {
+  interface Window {
+    iFrameResizer: IFramePageOptions
+    parentIFrame: IFramePage
   }
 }
--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -4,61 +4,59 @@
  * I'm not a TypeScript dev, so please feel free to submit PRs to improve this file.
  */
 
-declare module '@iframe-resizer/react' {
-  import * as React from 'react'
+import * as React from 'react'
 
-  export type IFrameObject = {
-    moveToAnchor: (anchor: string) => void
-    resize: () => void
-    sendMessage: (message: string, targetOrigin?: string) => void
-  }
-
-  export interface IFrameComponent extends HTMLIFrameElement {
-    iFrameResizer: IFrameObject
-  }
-
-  export type IFrameForwardRef = Omit<IFrameObject, 'close' | 'disconnect'> & {
-    getElement: () => IFrameComponent
-    getRef: () => any
-  }
-
-  export type IframeProps = React.DetailedHTMLProps<
-    React.IframeHTMLAttributes<HTMLIFrameElement>,
-    HTMLIFrameElement
-  >
-
-  export type ResizerOptions = {
-    bodyBackground?: string | null
-    bodyMargin?: string | number | null
-    bodyPadding?: string | number | null
-    checkOrigin?: boolean | string[]
-    direction?: 'vertical' | 'horizontal' | 'none'
-    forwardRef?: any
-    inPageLinks?: boolean
-    license: string
-    offset?: number
-    scrolling?: boolean | 'omit'
-    tolerance?: number
-    warningTimeout?: number
-  }
-
-  export type ResizerEvents = {
-    onInit?: (iframe: IFrameComponent) => void
-    onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void
-    onResized?: (ev: {
-      iframe: IFrameComponent
-      height: number
-      width: number
-      type: string
-    }) => void
-    onScroll?: (ev: { x: number; y: number }) => boolean
-  }
-
-  export type IframeResizerProps = Omit<IframeProps, 'scrolling'> &
-    ResizerOptions &
-    ResizerEvents
-
-  function IframeResizer(props: IframeResizerProps): React.ReactElement
-
-  export default IframeResizer
+export type IFrameObject = {
+  moveToAnchor: (anchor: string) => void
+  resize: () => void
+  sendMessage: (message: string, targetOrigin?: string) => void
 }
+
+export interface IFrameComponent extends HTMLIFrameElement {
+  iFrameResizer: IFrameObject
+}
+
+export type IFrameForwardRef = Omit<IFrameObject, 'close' | 'disconnect'> & {
+  getElement: () => IFrameComponent
+  getRef: () => any
+}
+
+export type IframeProps = React.DetailedHTMLProps<
+  React.IframeHTMLAttributes<HTMLIFrameElement>,
+  HTMLIFrameElement
+>
+
+export type ResizerOptions = {
+  bodyBackground?: string | null
+  bodyMargin?: string | number | null
+  bodyPadding?: string | number | null
+  checkOrigin?: boolean | string[]
+  direction?: 'vertical' | 'horizontal' | 'none'
+  forwardRef?: any
+  inPageLinks?: boolean
+  license: string
+  offset?: number
+  scrolling?: boolean | 'omit'
+  tolerance?: number
+  warningTimeout?: number
+}
+
+export type ResizerEvents = {
+  onInit?: (iframe: IFrameComponent) => void
+  onMessage?: (ev: { iframe: IFrameComponent; message: any }) => void
+  onResized?: (ev: {
+    iframe: IFrameComponent
+    height: number
+    width: number
+    type: string
+  }) => void
+  onScroll?: (ev: { x: number; y: number }) => boolean
+}
+
+export type IframeResizerProps = Omit<IframeProps, 'scrolling'> &
+  ResizerOptions &
+  ResizerEvents
+
+declare function IframeResizer(props: IframeResizerProps): React.ReactElement
+
+export default IframeResizer
