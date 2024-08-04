22f74ed62928aafa8b6b4ca2f786ba9063bfd9e7
David J. Bradshaw <dave@bradshaw.net>
2024-08-04 19:13:45 +0100

    Update notice

--- packages/legacy/js/notice.js
+++ packages/legacy/js/notice.js
@@ -2,11 +2,9 @@ const msg = `IFRAME-RESIZER 5
 
 This package has been split into separate packages for Parent and Child pages. Along with new packages for jQuery, React and Vue.
 
-Version 5 of iframe-resizer has been extensively rewritten to use modern browser APIs, which has enabled significantly better performance and greater accuracy in the detection of content resizing events.
-
-Going forward you are advised to use the separate packages for Parent and Child pages, rather than this combined package.
+Going forward you are advised to use the new separate packages for Parent and Child pages, rather than this combined package.
 
 Please see https://iframe-resizer.com/upgrade for more details.
 `
 
-console.warn(msg)
+console.info(msg)
