51101f8939778cf5173ff9cfc91682c3295117b0
David J. Bradshaw <dave@bradshaw.net>
2024-08-06 12:04:56 +0100

    Update examples

---
+++ example/vue/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import IframeResizer from '@iframe-resizer/vue/sfc'
+
+  const onReady = () => {
+    console.log('onReady')
+  }
+</script>
+
+<template>
+  <h1>Vue 3 + Iframe Resizer</h1>
+  <IframeResizer class="iframe" src="https://iframe-resizer.com/examples/animate/" license="GPLv3" bodyBackground="green" @on-ready="() => console.log('onReady')"/>
+</template>
+
+<style scoped>
+  .iframe {
+    width: 100% !important;
+    height: 100vh;
+  }
+</style>
