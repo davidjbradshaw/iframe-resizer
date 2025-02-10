f0a8ad6b2fc709904985d3e4eace2f607662212f
David J. Bradshaw <dave@bradshaw.net>
2025-02-10 13:54:58 +0000

    Tidy Quirks message

--- packages/child/index.js
+++ packages/child/index.js
@@ -196,12 +196,12 @@ function iframeResizerChild() {
   }
 
   function checkQuirksMode() {
-    if (document.compatMode !== 'BackCompat') return
+    if (document.compatMode === 'BackCompat') return
 
     advise(
       `<rb>Quirks Mode Detected</>
 
-The iframe is running in <b>Quirks Mode</>. This can cause issues with the correct operation of <i>iframe-resizer</>. It is recommended that you switch to <b>Standards Mode</>.
+This iframe is running in the browser's legacy <b>Quirks Mode</>, this may cause issues with the correct operation of <i>iframe-resizer</>. It is recommended that you switch to the modern <b>Standards Mode</>.
 
 For more information see <u>https://iframe-resizer.com/quirks-mode</>.
 `,
