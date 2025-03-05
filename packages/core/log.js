13ba192676a84b7ecf59fcc95bf157c22f082d0a
David J. Bradshaw <dave@bradshaw.net>
2025-03-05 12:43:11 +0000

    Add auto-group-console

--- packages/core/log.js
+++ packages/core/log.js
@@ -1,6 +1,7 @@
-import { BOLD } from '../common/consts'
+import { createGroupConsole } from 'auto-group-console'
+
+import { BOLD, TITLE } from '../common/consts'
 import formatAdvise from '../common/format-advise'
-import MicroConsole from '../common/micro-console'
 import { id as identity } from '../common/utils'
 
 const msgId = '[iframe-resizer]'
@@ -21,15 +22,17 @@ function getMyId(iframeId) {
 const isLogEnabled = (iframeId) =>
   settings[iframeId] ? settings[iframeId].log : logEnabled
 
-function setupMicroConsole(iframeId) {
+function setupConsole(iframeId) {
   settings[iframeId] = {
-    microConsole: MicroConsole(getMyId(iframeId)),
+    console: createGroupConsole({
+      title: `${TITLE}[${getMyId(iframeId)}]`,
+    }),
   }
 }
 
 export function setupLogging({ enabled, iframeId }) {
   logEnabled = enabled
-  setupMicroConsole(iframeId)
+  setupConsole(iframeId)
 }
 
 export function setLogSettings(newSettings) {
@@ -42,7 +45,7 @@ const formatLogMsg =
     [`${msgId}[${iframeId}]`, ...msg].join(' ')
 
 const output = (type, iframeId, ...msg) =>
-  settings[iframeId].microConsole[type](...msg)
+  settings[iframeId].console[type](...msg)
 
 export const log = (iframeId, ...msg) =>
   isLogEnabled(iframeId) === true ? output('log', iframeId, ...msg) : null
@@ -52,12 +55,14 @@ export const warn = (iframeId, ...msg) => output('warn', iframeId, ...msg)
 export const error = (iframeId, ...msg) => output('error', iframeId, ...msg)
 
 export const vInfo = (msg) =>
-  // eslint-disable-next-line no-console
-  console.info(`%c[iframe-resizer] ${msg}`, BOLD)
+  queueMicrotask(
+    // eslint-disable-next-line no-console
+    () => console.info(`%c[iframe-resizer] ${msg}`, BOLD),
+  )
 
 export const advise = (iframeId, msg) =>
   settings[iframeId]
-    ? settings[iframeId].microConsole.warn(formatAdvise(identity)(msg))
+    ? settings[iframeId].console.warn(formatAdvise(identity)(msg))
     : queueMicrotask(
         // eslint-disable-next-line no-console
         () => console?.warn(formatAdvise(formatLogMsg(iframeId))(msg)),
