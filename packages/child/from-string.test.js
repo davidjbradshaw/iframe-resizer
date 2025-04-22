06b2e31dff9283c0e2784e9b64b9eaaaad8cb616
David J. Bradshaw <dave@bradshaw.net>
2025-04-22 12:30:51 +0100

    Add tests

---
+++ packages/child/from-string.test.js
@@ -0,0 +1,36 @@
+import { getBoolean, getNumber } from './from-string'
+
+describe('from-string utility functions', () => {
+  describe('getBoolean', () => {
+    test('should return true for the string "true"', () => {
+      expect(getBoolean('true')).toBe(true)
+    })
+
+    test('should return false for the string "false"', () => {
+      expect(getBoolean('false')).toBe(false)
+    })
+
+    test('should return undefined for undefined input', () => {
+      expect(getBoolean()).toBeUndefined()
+    })
+
+    test('should return false for non-boolean strings', () => {
+      expect(getBoolean('random')).toBe(false)
+    })
+  })
+
+  describe('getNumber', () => {
+    test('should return a number for valid numeric strings', () => {
+      expect(getNumber('42')).toBe(42)
+      expect(getNumber('3.14')).toBe(3.14)
+    })
+
+    test('should return NaN for non-numeric strings', () => {
+      expect(getNumber('random')).toBeNaN()
+    })
+
+    test('should return undefined for undefined input', () => {
+      expect(getNumber()).toBeUndefined()
+    })
+  })
+})
