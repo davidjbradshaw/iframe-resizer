9a12b2f78eed3aed37a5fe18e460ecb275b55937
David J. Bradshaw <dave@bradshaw.net>
2025-04-24 11:59:06 +0100

    Add comment

--- packages/child/index.js
+++ packages/child/index.js
@@ -1521,6 +1521,7 @@ This version of <i>iframe-resizer</> can auto detect the most suitable ${type} c
       },
 
       resize() {
+        // This method is used by the tabVisible event on the parent page
         log('Resize requested by host page')
         sendSize(PARENT_RESIZE_REQUEST, 'Parent window requested size check')
       },
