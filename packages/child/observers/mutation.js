6fbeddd53f9e8d4d1a090e46d62876b5c9a8bbea
David J. Bradshaw <dave@bradshaw.net>
2025-07-25 18:30:49 +0100

    PR fix

--- packages/child/observers/mutation.js
+++ packages/child/observers/mutation.js
@@ -14,7 +14,7 @@ let pending = false
 let perfMon = 0
 
 const updateMutation = (mutations) => {
-  info('Mutations:', mutations)
+  log('Mutations:', mutations)
 
   for (const mutation of mutations) {
     const { addedNodes, removedNodes } = mutation
