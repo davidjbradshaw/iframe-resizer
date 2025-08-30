37a53eee3f827e361ad44c4f36c029419dc7a18c
David J. Bradshaw <dave@bradshaw.net>
2025-08-30 09:11:17 +0100

    Tidy

--- jest.setup.js
+++ jest.setup.js
@@ -3,14 +3,3 @@ import $ from 'jquery'
 
 global.$ = $
 global.jQuery = $
-
-// jest.mock('auto-console-group', () => {
-//   // Default export: factory returning a console-group-like API (no-ops)
-//   const create = () => new Proxy({}, { get: () => () => {} })
-
-//   // Named export used by your code
-//   const NORMAL = 'font-weight: normal;'
-
-//   // ESM-compatible mock (default + named)
-//   return { __esModule: true, default: create, NORMAL }
-// })
