1f8b95c4395787669cea07fe87585c95149a7779
David J. Bradshaw <dave@bradshaw.net>
2025-08-30 09:10:23 +0100

    Fix Jest for ACG

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,16 @@
-import { jest } from '@jest/globals'
+// import { jest } from '@jest/globals'
 import $ from 'jquery'
 
 global.$ = $
 global.jQuery = $
 
-jest.mock('auto-console-group', () => ({
-  createGroupConsole: () => console,
-}))
+// jest.mock('auto-console-group', () => {
+//   // Default export: factory returning a console-group-like API (no-ops)
+//   const create = () => new Proxy({}, { get: () => () => {} })
+
+//   // Named export used by your code
+//   const NORMAL = 'font-weight: normal;'
+
+//   // ESM-compatible mock (default + named)
+//   return { __esModule: true, default: create, NORMAL }
+// })
