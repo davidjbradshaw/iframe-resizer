b45390523957fc6d159a92676e5715aac0704aa6
David J. Bradshaw <dave@bradshaw.net>
2025-08-31 09:49:57 +0100

    Extract decode from parent

--- packages/core/index.js
+++ packages/core/index.js
@@ -60,6 +60,7 @@ import {
   vInfo,
   warn,
 } from './console'
+import decodeMessage from './decode'
 import createOutgoingMessage from './outgoing'
 import {
   getPagePosition,
@@ -79,52 +80,6 @@ function iframeListener(event) {
     setPagePosition(iframeId)
 
     on('onResized', messageData)
-  }
-
-  function getPaddingEnds(compStyle) {
-    if (compStyle.boxSizing !== 'border-box') return 0
-
-    const top = compStyle.paddingTop ? parseInt(compStyle.paddingTop, 10) : 0
-    const bot = compStyle.paddingBottom
-      ? parseInt(compStyle.paddingBottom, 10)
-      : 0
-
-    return top + bot
-  }
-
-  function getBorderEnds(compStyle) {
-    if (compStyle.boxSizing !== 'border-box') return 0
-
-    const top = compStyle.borderTopWidth
-      ? parseInt(compStyle.borderTopWidth, 10)
-      : 0
-
-    const bot = compStyle.borderBottomWidth
-      ? parseInt(compStyle.borderBottomWidth, 10)
-      : 0
-
-    return top + bot
-  }
-
-  function decodeMessage(msg) {
-    const data = msg.slice(MESSAGE_ID_LENGTH).split(':')
-    const height = data[1] ? Number(data[1]) : 0
-    const iframe = settings[data[0]]?.iframe
-    const compStyle = getComputedStyle(iframe)
-
-    const messageData = {
-      iframe,
-      id: data[0],
-      height: height + getPaddingEnds(compStyle) + getBorderEnds(compStyle),
-      width: Number(data[2]),
-      type: data[3],
-      msg: data[4],
-    }
-
-    // eslint-disable-next-line prefer-destructuring
-    if (data[5]) messageData.mode = data[5]
-
-    return messageData
   }
 
   function isMessageFromIframe() {
