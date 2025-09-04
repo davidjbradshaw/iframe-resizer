9c6b5cddbca93018561894953eff3a2931b98949
David J. Bradshaw <dave@bradshaw.net>
2025-09-04 18:28:55 +0100

    delint

--- packages/core/index.js
+++ packages/core/index.js
@@ -1330,7 +1330,11 @@ function sendTriggerMsg(eventName, event) {
 function tabVisible() {
   if (document.hidden === false) {
     consoleEvent('document', 'visibilityChange')
-    log('document', 'Visibility Change:', document.hidden ? 'hidden' : 'visible')
+    log(
+      'document',
+      'Visibility Change:',
+      document.hidden ? 'hidden' : 'visible',
+    )
     sendTriggerMsg('tabVisible', RESIZE)
   }
 }
