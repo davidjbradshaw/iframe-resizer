2dcb97478e1e0ebffdc98404b7ef651b1e0f72ee
David J. Bradshaw <dave@bradshaw.net>
2025-10-02 13:37:05 +0100

    Fix: Only encode strings

--- packages/common/format-advise.js
+++ packages/common/format-advise.js
@@ -1,11 +1,15 @@
+import { STRING } from './consts'
+
 const encode = (s) =>
-  s
-    .replaceAll('<br>', '\n')
-    .replaceAll('<rb>', '\u001B[31;1m')
-    .replaceAll('</>', '\u001B[m')
-    .replaceAll('<b>', '\u001B[1m')
-    .replaceAll('<i>', '\u001B[3m')
-    .replaceAll('<u>', '\u001B[4m')
+  typeof s === STRING
+    ? s
+        .replaceAll('<br>', '\n')
+        .replaceAll('<rb>', '\u001B[31;1m')
+        .replaceAll('</>', '\u001B[m')
+        .replaceAll('<b>', '\u001B[1m')
+        .replaceAll('<i>', '\u001B[3m')
+        .replaceAll('<u>', '\u001B[4m')
+    : s
 
 const remove = (s) => s.replaceAll('<br>', '\n').replaceAll(/<[/a-z]+>/gi, '')
 
