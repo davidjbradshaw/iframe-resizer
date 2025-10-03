b7aca2f3171a7e60bf6aba5634928030bf04b9c8
David J. Bradshaw <dave@bradshaw.net>
2025-10-03 17:10:18 +0100

    Store advise tags in an object

--- packages/common/format-advise.js
+++ packages/common/format-advise.js
@@ -1,23 +1,38 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-useless-escape */
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable security/detect-non-literal-regexp */
+
 import { NEW_LINE } from './consts'
 import { isString } from './utils'
 
-const encode = (s) =>
-  s
-    .replaceAll('<br>', NEW_LINE)
-    .replaceAll('<rb>', '\u001B[31;1m')
-    .replaceAll('<bb>', '\u001B[34;1m')
-    .replaceAll('</>', '\u001B[m')
-    .replaceAll('<b>', '\u001B[1m')
-    .replaceAll('<i>', '\u001B[3m')
-    .replaceAll('<u>', '\u001B[4m')
+const TAGS = {
+  br: '\n',
+  rb: '\u001B[31;1m', // red bold
+  bb: '\u001B[34;1m', // blue bold
+  b: '\u001B[1m', // bold
+  i: '\u001B[3m', // italic
+  u: '\u001B[4m', // underline
+  '/': '\u001B[m', // reset
+}
+
+// <tag> where tag is one of our keys
+const tags = new RegExp(
+  `<(\/|${Object.keys(TAGS)
+    .filter((tag) => tag !== '/')
+    .join('|')})>`,
+  'gi',
+)
+
+const encode = (s) => s.replace(tags, (_, name) => TAGS[name] ?? '')
 
 const filter = (s) =>
   s.replaceAll('<br>', NEW_LINE).replaceAll(/<\/?[^>]+>/gi, '')
 
 const ifString = (process) => (s) => (isString(s) ? process(s) : s)
 
+// Chrome shows ANSI; others get plain text
 export default (formatLogMessage) => (message) =>
-  // Only show formatting in Chrome as not supported in other browsers
   formatLogMessage(
     window.chrome ? ifString(encode)(message) : ifString(filter)(message),
   )
