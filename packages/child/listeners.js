329c2bdc3a685432c6d2f64824a056b7c9e956bd
David J. Bradshaw <dave@bradshaw.net>
2025-10-17 12:04:32 +0100

    Make 'once' default on event listeners

--- packages/child/listeners.js
+++ packages/child/listeners.js
@@ -2,19 +2,20 @@ import { HIGHLIGHT } from 'auto-console-group'
 
 import { log } from './console'
 
+const ONCE = { once: true }
 export const tearDownList = []
 
 function logEvent(type, evt) {
   log(`${type} event listener: %c${evt}`, HIGHLIGHT)
 }
 
-export const removeEventListener = (el, evt, func) => {
-  el.removeEventListener(evt, func, false)
+export const removeEventListener = (el, evt, func, options) => {
+  el.removeEventListener(evt, func, options)
   logEvent('Remove', evt)
 }
 
-export const addEventListener = (el, evt, func, options) => {
-  el.addEventListener(evt, func, options || false)
-  tearDownList.push(() => removeEventListener(el, evt, func))
+export const addEventListener = (el, evt, func, options = ONCE) => {
+  el.addEventListener(evt, func, options)
+  tearDownList.push(() => removeEventListener(el, evt, func, options))
   logEvent('Add', evt)
 }
