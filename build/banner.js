1eab6bbccfb2195012416a4bbb02d29856de76bd
David J. Bradshaw <dave@bradshaw.net>
2025-10-23 15:04:05 +0100

    Update banner

--- build/banner.js
+++ build/banner.js
@@ -6,19 +6,19 @@ const today = date.toISOString().split('T')[0]
 
 export default (file, type) => `/*!
  *  @preserve
- *  
+ *
  *  @module      iframe-resizer/${file} ${pkg.version} (${type}) ${type === 'iife' ? '' : `- ${today}`}
  *
- *  @license     ${pkg.license} for non-commercial use only.
- *               For commercial use, you must purchase a license from
+ *  @license     ${pkg.license} For use with GPL compliant sites (fully published front & backend source code)
+ *               Alternatively for commercial use, you can purchase a license from
  *               ${pkg.homepage}/pricing
- * 
- *  @description Keep same and cross domain iFrames sized to their content 
+ *
+ *  @description Keep same and cross domain iFrames sized to their content
  *
  *  @author      ${pkg.author.name} <${pkg.author.email}>
- * 
+ *
  *  @see         {@link ${pkg.homepage}}
- * 
+ *
  *  @copyright  (c) 2013 - ${year}, ${pkg.author.name}. All rights reserved.
  */
 
