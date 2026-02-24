a9447d2ca3b4d7e42c133e982b04adfbe695c1ee
David J. Bradshaw <dave@bradshaw.net>
2026-02-24 11:43:20 +0000

    delint

--- packages/svelte/index.js
+++ packages/svelte/index.js
@@ -1,4 +1,5 @@
 import IframeResizer from './IframeResizer.svelte'
 
+// `export { default }` is blocked by no-restricted-exports, so we import and re-export
 // eslint-disable-next-line unicorn/prefer-export-from
 export default IframeResizer
