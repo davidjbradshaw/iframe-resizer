38f89ec0562e09ee6a75e43cfbd42c6d79f1c5b9
David J. Bradshaw <dave@bradshaw.net>
2026-04-08 12:25:08 +0100

    Remove legacy package

--- packages/legacy/js/iframeResizer.contentWindow.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-describe('legacy/js/iframeResizer.contentWindow', () => {
-  it('imports child/index without error', async () => {
-    const mod = await import('./iframeResizer.contentWindow.js')
-    expect(mod).toBeDefined()
-  })
-})
