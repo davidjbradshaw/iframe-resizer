4fdeb1d1f0a676801228ff4477e216896f086c25
David J. Bradshaw <dave@bradshaw.net>
2026-04-13 16:03:34 +0100

    .

--- example/react/vite.config.js
+++ example/react/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/example/react/dist/',
+  resolve: {
+    preserveSymlinks: true,
+  },
 })
--- example/vue/vite.config.js
+++ example/vue/vite.config.js
@@ -5,4 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/example/vue/dist/',
+  resolve: {
+    preserveSymlinks: true,
+  },
 })
