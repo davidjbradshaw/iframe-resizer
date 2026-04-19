27b94c6378c5369f90c421d927845cd2a499b203
David J. Bradshaw <dave@bradshaw.net>
2026-04-19 12:50:02 +0100

    tidy

--- packages/child/methods/get-version.ts
+++ packages/child/methods/get-version.ts
@@ -1,13 +1,11 @@
 import type { IFrameVersion } from '@iframe-resizer/common'
-import { FALSE, VERSION } from '@iframe-resizer/common/consts'
+import { VERSION } from '@iframe-resizer/common/consts'
 
 import settings from '../values/settings'
 
 export default function getVersion(): IFrameVersion {
-  const { version } = settings
-
   return {
     child: VERSION,
-    parent: !version || version === FALSE ? 'legacy' : version,
+    parent: settings.version || 'legacy',
   }
 }
