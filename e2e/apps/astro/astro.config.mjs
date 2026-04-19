547c93c95692d4fb391d6918d7ac5b64ad6c5413
David J. Bradshaw <dave@bradshaw.net>
2026-04-19 06:51:14 +0100

    fixes

--- e2e/apps/astro/astro.config.mjs
+++ e2e/apps/astro/astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   base: '/e2e/fixtures/astro',
   outDir: '../../fixtures/astro',
+  vite: {
+    resolve: {
+      preserveSymlinks: true,
+    },
+  },
 })
