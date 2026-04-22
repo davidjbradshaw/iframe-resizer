7ffeb4cb60393e115993a0102c96937e46df9fa0
David J. Bradshaw <dave@bradshaw.net>
2026-04-22 11:53:01 +0100

    Web Component example

--- example/web-component/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-import serveChild from '../shared/serve-child.js'
-
-export default defineConfig({
-  plugins: [serveChild()],
-  base: '/example/web-component/dist/',
-})
